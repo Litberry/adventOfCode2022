f7c5c6386f8a069f3f61d69c3ee9a92effb51b0b
Litberry <starythehappystar@gmail.com>
2022-12-11 11:21:15 -0500

    Day 3 Completed!

--- Day3/rucksackFind3.py
+++ Day3/rucksackFind3.py
@@ -1,6 +1,6 @@
-def find3(word1, bigString): # Finds the common character common in all 3 strings
+def find3(w1, w2, w3): # Finds the common character common in all 3 strings
     for ch in word1:
-        if bigString.count(ch) == 3:
+        if w2.find(ch) > -1 and w3.find(ch) > -1:
             return ch
 
 file1 = open("dataf.txt", "r+")
@@ -10,20 +10,34 @@ Split string in half
 '''
     
 score = 0
-bigWord = ''
 count = 0
 
-for row in file1:
-    comp1 = row
-    bigWord += row[0:len(comp1)-1]
-    count += 1
-    if count == 3:
-        ch = find3(comp1, bigWord)
-        if ch.isupper():
-            score += ord(ch) - ord('A') + 27
-        else:
-            score += ord(ch) - ord('a') + 1
+# for row in file1:
+#     comp1 = row
+#     bigWord += row[0:len(comp1)-1]
+#     count += 1
+#     if count == 3:
+#         print(len(file1))
+#         count = 0
+        # ch = find3(comp1, bigWord)
+        # if ch.isupper():
+        #     score += ord(ch) - ord('A') + 27
+        # else:
+        #     score += ord(ch) - ord('a') + 1
+        # bigWord = ''
 
+rucksacks = file1.readlines()
+count = 0
+while count < len(rucksacks):
+    word1 = rucksacks[count]
+    word2 = rucksacks[count+1]
+    word3 = rucksacks[count+2]
+    count += 3
+    ch = find3(word1, word2, word3)
+    if ch.isupper():
+        score += ord(ch) - ord('A') + 27
+    else:
+        score += ord(ch) - ord('a') + 1
 
 print(score)
 
